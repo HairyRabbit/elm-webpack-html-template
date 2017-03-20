74642b3393ec6d3f9cf230b57eea09b46a0f6a1c
yuffiy <yfhj1990@hotmail.com>
2017-03-20 09:29:01 +0800

    bump version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # elm-webpack-html-template
+
+
+```js
+"start": "cross-env NODE_ENV=development ./node_modules/.bin/webpack-dev-server src/Main.elm ./node_modules/elm-webpack-html-template/patch -d --module-bind=elm=elm-webpack-loader --module-bind=css=style-loader!css-loader --plugin=html-webpack-plugin?template=./node_modules/html-webpack-template/index.ejs,mobile=true,inject=false,appMountId=app --history-api-fallback --host=0.0.0.0 --output-public-path=/"
+```
+
--- patch.js
+++ patch.js
@@ -1,5 +1,5 @@
-Promise.all([
-    import(`${process.cwd}Application/Main.elm`),
-    import('normalize.css'),
-    import(`style-loader!css-loader!elm-css-webpack-loader!${process.cwd}Application/Stylesheets.elm`)
-]).then(([App]) => App.Main.embed(document.getElementById('app')))
+import App from '../../Application/Main.elm'
+import 'normalize.css'
+import 'style-loader!css-loader!elm-css-webpack-loader!../../Application/Stylesheets.elm'
+
+App.Main.embed(document.getElementById('app'))
